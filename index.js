bfacdd165c6dc618c617432bc551716c65146544
Yuri Zakharov <yuri.zaharov@gmail.com>
2023-01-09 18:55:25 +0300

    Changes for testing env variables

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const packJson = require('./package.json');
 
 const app = express();
 
+testVar = process.env.TEST_VAR || 'Value from code'
+
 app
     .get('/ping', function(req, res) {
         res
@@ -14,6 +16,12 @@ app
         res
             .status(200)
             .send({'version': packJson.version});
+    })
+
+    .get('/variable', function(req, res) {
+        res
+            .status(200)
+            .send({'testVar': testVar});
     })
 
 app.listen(8080, (err) => {
